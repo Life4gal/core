02c12a3566b4a89d542ed50d181ea78ba0916db3
Life4gal <life4gal@gmail.com>
2026-01-13 15:46:22 +0800

    test

--- test/main.cpp
+++ test/main.cpp
@@ -1,1 +1,132 @@
-auto main() noexcept -> int {}
+#include <prometheus/ut/unit_test.hpp>
+#include <prometheus/command_line_parser/command_line_parser.hpp>
+
+using namespace prometheus;
+
+auto main() noexcept -> int
+{
+	using parser_type = clp::OptionParser<>;
+	using option_type = parser_type::option_type;
+
+	parser_type parser{};
+
+	parser
+			.add_option("tab-width", option_type::implicit_value("4"))
+			.add_option("max-failures", option_type::implicit_value("100"))
+			.add_option("dry,dry-run", option_type::default_value("true"))
+			.add_option("exec-suite-name")
+			.add_option("exec-test-name")
+			.add_option("call-debugger-if-fail", option_type::default_value("false"))
+			.add_option("call-debugger-if-fatal", option_type::default_value("true"))
+			//
+			.add_alias("x-fail", "call-debugger-if-fail")
+			.add_alias("x-fatal", "call-debugger-if-fatal")
+			//
+			;
+
+	auto& config = ut::get_config();
+	// config.report_level = ut::config_type::ReportLevel::ALL;
+
+	try
+	{
+		parser.parse();
+	}
+	catch (const platform::IException& exception)
+	{
+		exception.print();
+		config.dry_run = true;
+		return 1;
+	}
+
+	const auto tab_width = parser["tab-width"].as<std::size_t>();
+	const auto max_failures = parser["max-failures"].as<std::size_t>();
+	const auto dry_run = parser["dry-run"].as<bool>();
+	const auto call_debugger_if_fail = parser["call-debugger-if-fail"].as<bool>();
+	const auto call_debugger_if_fatal = parser["call-debugger-if-fatal"].as<bool>();
+
+	if (tab_width.has_value())
+	{
+		config.tab_width = *tab_width;
+	}
+	if (max_failures.has_value())
+	{
+		config.abort_after_n_failures = *max_failures;
+	}
+	if (dry_run.has_value())
+	{
+		config.dry_run = *dry_run;
+	}
+	if (call_debugger_if_fail.has_value())
+	{
+		if (*call_debugger_if_fail)
+		{
+			config.break_point_level |= ut::config_type::BreakPointLevel::FAILURE;
+		}
+		else
+		{
+			config.break_point_level &= ~ut::config_type::BreakPointLevel::FAILURE;
+		}
+	}
+	if (call_debugger_if_fatal.has_value())
+	{
+		if (*call_debugger_if_fatal)
+		{
+			config.break_point_level |= ut::config_type::BreakPointLevel::FATAL;
+		}
+		else
+		{
+			config.break_point_level &= ~ut::config_type::BreakPointLevel::FATAL;
+		}
+	}
+
+	if (const auto& option = parser["exec-suite-name"];
+		option.set())
+	{
+		if (const auto wildcard = option.as<std::string_view>();
+			wildcard.has_value() and *wildcard == "*")
+		{
+			config.filter_suite = [](const ut::suite_node_type& node) noexcept -> bool
+			{
+				std::ignore = node;
+				return true;
+			};
+		}
+		else if (auto suite_names = option.as<std::vector<std::string>>();
+			suite_names.has_value())
+		{
+			config.filter_suite = [suite = *std::move(suite_names)](const ut::suite_node_type& node) noexcept -> bool
+			{
+				return std::ranges::contains(suite, node.name);
+			};
+		}
+	}
+
+	if (const auto& option = parser["exec-test-name"];
+		option.set())
+	{
+		if (const auto wildcard = option.as<std::string_view>();
+			wildcard.has_value() and *wildcard == "*")
+		{
+			config.filter_test = [](const ut::test_node_type& node) noexcept -> bool
+			{
+				std::ignore = node;
+				return true;
+			};
+		}
+		else if (auto test_names = option.as<std::vector<std::string>>();
+			test_names.has_value())
+		{
+			config.filter_test =
+					[test = *std::move(test_names)](const ut::test_node_type& node) noexcept -> bool
+					{
+						if (node.parent != nullptr)
+						{
+							// child
+							return true;
+						}
+
+						return std::ranges::contains(test, node.name) and not std::ranges::contains(node.categories.get(), "skip");
+					};
+		}
+	}
+}
